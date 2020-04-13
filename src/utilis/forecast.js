63458cd945e070b958dc08bed062812bb6f7e71f
AmiraAmr <amiraamr1538@gmail.com>
2020-04-13 23:24:56 +0200

    edits in forecast

--- src/utilis/forecast.js
+++ src/utilis/forecast.js
@@ -1,5 +1,6 @@
 const request = require('request');
 
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/b7d4f09a0b230d0475b4732ecfffa9d1/' + latitude + ',' + longitude + '?units=si';
 
@@ -8,12 +9,13 @@ const forecast = (latitude, longitude, callback) => {
         //also using url directly instead url: url(the var) as they have the same name
 
         if(error) {
-            callback("NO CONNECTION !", undefined);
+            callback("THERE IS NO CONNECTION !", undefined);
             
         } else if (body.error ) {
-            callback("Unable to find location. Try another search !", undefined);
+            callback("Unable to find this location. Try another search !", undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + "and it is " + body.currently.temperature + " degrees with " + body.currently.precipProbability + "% chance to rain")
+            console.log(body.daily.data[0]);
+            callback(undefined, body.daily.data[0].summary + ". It is " + body.currently.temperature + " degrees ( high today is " + body.daily.data[0].temperatureHigh + " & low of " + body.daily.data[0].temperatureLow +"), With " + body.currently.precipProbability + "% chance to rain")
         }
     })
 }
