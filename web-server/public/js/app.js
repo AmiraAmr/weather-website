4c2893a66f9c282b5a0b48c049f4d3a4624f9a35
AmiraAmr <amiraamr1538@gmail.com>
2020-04-13 03:59:48 +0200

    Remove unnecssary console.log calls

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -20,7 +20,6 @@ weatherForm.addEventListener('submit', (e) => {
         } else {
             messageOne.textContent = data.location + " : " + data.forecast
         }
-        
         })
     })
     
